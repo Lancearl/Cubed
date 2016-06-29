71e7b375dc2a203843648b83999d99898b66ac4f
Lancearl <subwoofer-d@hotmail.com>
2016-06-29 21:03:38 +1200

    Updated loader to register normals (and uvs)
    The loader now separates out normals and uvs. Will cause buggy behaviour
    at the moment if trying to load an obj without uvs so stick with cube2.
    Was working with lighting on my older version of the project but I'll
    need to look into making it work with the current version. So basically
    at the moment it doesn't look like I've changed anything :D

--- Mesh.h
+++ Mesh.h
@@ -14,20 +14,9 @@ public:
 	Mesh();
 	~Mesh();
 
-	std::vector < GLfloat > vertices;
-	std::vector < GLfloat > uvs;
-	std::vector < GLfloat > normals;
-	std::vector <unsigned int> idx;
-
+	std::vector < GLfloat > vertices, uvs, normals;
 	std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
 
 	void draw();
 	bool load(const char* filename);
-
-	///Do not delete:
-	//bool load(const char* filename, 
-	//	std::vector < sf::Vector3f > & out_vertices,
-	//	std::vector < sf::Vector2f > & out_uvs,
-	//	std::vector < sf::Vector3f > & out_normals);
 };
-
